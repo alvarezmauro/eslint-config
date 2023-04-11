bd3e4660c80f82871b29aee209a4ce68615f8fde
Mauro Alvarez <contact@mauroalvarez.com>
2023-04-11 00:55:02 -0300

    Add typescript rules

---
+++ rules/typescript.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "@typescript-eslint/await-thenable": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+
+    // This allows us to use async function on addEventListener(). Discussion: https://twitter.com/wesbos/status/1337074242161172486
+    "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+            checksVoidReturn: false,
+        },
+    ],
+};
