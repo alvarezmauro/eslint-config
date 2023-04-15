fb5832cfb0f79777c590e7fa3164b6017f4715e1
Mauro Alvarez <contact@mauroalvarez.com>
2023-04-15 20:10:59 -0300

    Rename and update prettier and typescript setup files

---
+++ commands/setup-prettier.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const PrettyConsole = require('../lib/prettyConsole');
+const PRETTIER_CONFIG = require('../.prettierrc.js');
+
+const PROJECT_PRETTIER_CONFIG_PATH = `${process.cwd()}/.prettierrc`;
+const PROJECT_PRETTIER_SAMPLE_CONFIG_PATH = `${process.cwd()}/.prettierrc.sample`;
+
+/**
+ * Function to setup prettier configuration
+ *
+ * @param {string} option - Selected option, can be 'override', 'merge', 'create', 'sample'
+ */
+function setupPrettier(option) {
+    const prettyConsole = new PrettyConsole();
+    prettyConsole.closeByNewLine = true;
+    prettyConsole.useIcons = true;
+
+    prettyConsole.info('Setting up prettier configuration');
+
+    let prettierConfigExists = false;
+    let projectPrettierConfig = {};
+
+    if (option === 'override' || option === 'create') {
+        fs.writeFileSync(
+            PROJECT_PRETTIER_CONFIG_PATH,
+            JSON.stringify(PRETTIER_CONFIG, null, 4),
+        );
+    } else if (option === 'merge') {
+        try {
+            projectPrettierConfig = JSON.parse(
+                fs.readFileSync(PROJECT_PRETTIER_CONFIG_PATH),
+            );
+            prettierConfigExists = true;
+        } catch (err) {
+            prettyConsole.error('.prettierrc file not found');
+            prettyConsole.info('We will create a new one for you');
+        }
+
+        const mergedPrettierConfig = {
+            ...PRETTIER_CONFIG,
+            ...projectPrettierConfig,
+        };
+
+        fs.writeFileSync(
+            PROJECT_PRETTIER_CONFIG_PATH,
+            JSON.stringify(mergedPrettierConfig, null, 4),
+        );
+    } else if (option === 'sample') {
+        fs.writeFileSync(
+            PROJECT_PRETTIER_SAMPLE_CONFIG_PATH,
+            JSON.stringify(PRETTIER_CONFIG, null, 4),
+        );
+    }
+
+    console.log(`${prettierConfigExists ? 'Updated' : 'Created'} .prettierrc`);
+}
+
+module.exports = setupPrettier;
---
+++ commands/setup-typescript.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const PrettyConsole = require('../lib/prettyConsole');
+const TSCONFIG = require('../tsconfig.json');
+const PROJECT_TYPESCRIPT_CONFIG_PATH = `${process.cwd()}/tsconfig.json`;
+const PROJECT_TYPESCRIPT_SAMPLE_CONFIG_PATH = `${process.cwd()}/.prettierrc.sample`;
+
+/**
+ * Function to setup TypeScript configuration
+ *
+ * @param {string} option - Selected option, can be 'override', 'merge', 'create', 'sample'
+ */
+function updateTsconfig(option) {
+    const prettyConsole = new PrettyConsole();
+    prettyConsole.closeByNewLine = true;
+    prettyConsole.useIcons = true;
+
+    prettyConsole.info('Setting up TypeScript');
+
+    let tsConfigExists = false;
+    let projectTsConfig = {};
+
+    if (option === 'override' || option === 'create') {
+        fs.writeFileSync(
+            PROJECT_TYPESCRIPT_CONFIG_PATH,
+            JSON.stringify(TSCONFIG, null, 4),
+        );
+    } else if (option === 'merge') {
+        try {
+            projectTsConfig = JSON.parse(
+                fs.readFileSync(PROJECT_TYPESCRIPT_CONFIG_PATH),
+            );
+            tsConfigExists = true;
+        } catch (err) {
+            prettyConsole.error('tsconfig.json file not found');
+            prettyConsole.info('We will create a new one for you');
+        }
+
+        const mergedTsConfig = {
+            ...TSCONFIG,
+            ...projectTsConfig,
+        };
+
+        fs.writeFileSync(
+            PROJECT_TYPESCRIPT_CONFIG_PATH,
+            JSON.stringify(mergedTsConfig, null, 4),
+        );
+    } else if (option === 'sample') {
+        fs.writeFileSync(
+            PROJECT_TYPESCRIPT_SAMPLE_CONFIG_PATH,
+            JSON.stringify(TSCONFIG, null, 4),
+        );
+    }
+}
+
+module.exports = updateTsconfig;
