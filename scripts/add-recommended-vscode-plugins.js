7852d978d0b93768e5c63f9eb7de17081d1bd35c
Mauro Alvarez <contact@mauroalvarez.com>
2023-04-20 14:24:25 -0300

    Add .vscode/extensions.json and add-recommended-vscode-plugins script

---
+++ scripts/add-recommended-vscode-plugins.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+
+const VSCODE_EXTENSIONS_PATH = path.resolve(
+    process.cwd(),
+    '.vscode/extensions.json',
+);
+
+const REQUIRED_EXTENSIONS = [
+    'esbenp.prettier-vscode',
+    'dbaeumer.vscode-eslint',
+];
+
+async function addRecommendedVscodePlugins() {
+    const { addRecommendedVscodePlugins } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'addRecommendedVscodePlugins',
+            message:
+                'Do you want to add recommended VSCode plugins to your project .vscode/extensions.json?',
+            choices: [
+                { name: 'Yes', value: true },
+                { name: 'No', value: false },
+            ],
+        },
+    ]);
+
+    if (!addRecommendedVscodePlugins) {
+        return;
+    }
+
+    // Check if the extensions.json file exists
+    if (fs.existsSync(VSCODE_EXTENSIONS_PATH)) {
+        // Read the file and parse its contents as JSON
+        const vscodeExtensions = JSON.parse(
+            fs.readFileSync(VSCODE_EXTENSIONS_PATH, 'utf8'),
+        );
+
+        // Check if the recommendations attribute exists and contains the required extensions
+        if (
+            vscodeExtensions.recommendations &&
+            requiredExtensions.every((ext) =>
+                vscodeExtensions.recommendations.includes(ext),
+            )
+        ) {
+            prettyConsole.info(
+                `The ".vscode/extensions.json" file already contains the recommended plugins: ${requiredExtensions.join(
+                    ', ',
+                )}`,
+            );
+        } else {
+            // Add the required extensions to the recommendations attribute
+            if (!vscodeExtensions.recommendations) {
+                vscodeExtensions.recommendations = [];
+            }
+            requiredExtensions.forEach((ext) => {
+                if (!vscodeExtensions.recommendations.includes(ext)) {
+                    vscodeExtensions.recommendations.push(ext);
+                }
+            });
+
+            // Write the updated JSON object back to the file
+            fs.writeFileSync(
+                VSCODE_EXTENSIONS_PATH,
+                JSON.stringify(vscodeExtensions, null, 4),
+            );
+
+            prettyConsole.info(
+                `The ".vscode/extensions.json" file has been updated with the following plugins recommendations: ${requiredExtensions.join(
+                    ', ',
+                )}`,
+            );
+        }
+    } else {
+        // Create a new .vscode/extensions.json file with the plugin recommendations
+        const json = {
+            recommendations: requiredExtensions,
+            unwantedRecommendations: [],
+        };
+
+        // Write the JSON object to the file
+        fs.writeFileSync(filepath, JSON.stringify(json, null, 4));
+        prettyConsole.info(
+            `The ".vscode/extensions.json" file has been created with the following recommendations: ${requiredExtensions.join(
+                ', ',
+            )}`,
+        );
+    }
+}
+
+module.exports = addRecommendedVscodePlugins;
