c814aabe1115daa5f2f4efac4d33bd270661d9f7
Mauro Alvarez <contact@mauroalvarez.com>
2023-04-21 01:28:17 -0300

    Update add-recommended-vscode-plugin script

--- scripts/add-recommended-vscode-plugins.js
+++ scripts/add-recommended-vscode-plugins.js
@@ -3,10 +3,8 @@ const path = require('path');
 const inquirer = require('inquirer');
 const PrettyConsole = require('../lib/prettyConsole');
 
-const VSCODE_EXTENSIONS_PATH = path.resolve(
-    process.cwd(),
-    '.vscode/extensions.json',
-);
+const VSCODE_FOLDER_PATH = path.resolve(process.cwd(), '.vscode');
+const VSCODE_EXTENSIONS_PATH = `${VSCODE_FOLDER_PATH}/extensions.json`;
 
 const REQUIRED_EXTENSIONS = [
     'esbenp.prettier-vscode',
@@ -83,6 +81,10 @@ async function addRecommendedVscodePlugins() {
             recommendations: REQUIRED_EXTENSIONS,
             unwantedRecommendations: [],
         };
+
+        if (!fs.existsSync(VSCODE_FOLDER_PATH)) {
+            fs.mkdirSync(VSCODE_FOLDER_PATH);
+        }
 
         // Write the JSON object to the file
         fs.writeFileSync(VSCODE_EXTENSIONS_PATH, JSON.stringify(json, null, 4));
